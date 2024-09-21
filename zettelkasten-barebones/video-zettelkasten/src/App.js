abbafb27fae9cfb7a5dec97f6437becd11e6fbdc
Yash Raj Singh <yashudps.ys@gmail.com>
2024-09-21 15:52:37 +0530

    feat: zettlekasten

---
+++ zettelkasten-barebones/video-zettelkasten/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Zettelkasten from "./components/Zettelkasten";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Zettelkasten />
+    </div>
+  );
+}
+
+export default App;
